37c79522714cfae23faed38e606e7066e62f9691
fauzanpahlawan <fauzan.pahlawan@gmail.com>
2017-11-27 09:02:19 +0700

    commit2

--- app/src/main/java/com/example/fauza/mymdb/MovieAdapter.java
+++ app/src/main/java/com/example/fauza/mymdb/MovieAdapter.java
@@ -1,4 +1,33 @@
 package com.example.fauza.mymdb;
 
-public class MovieAdapter  {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieViewHolder> {
+    private Context mContext;
+    // TODO Make ArrayList of Movie Poster
+    public MovieAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.item_view_movie, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        //TODO Bind MoviePoster from ArrayList to ImageView based on Position
+    }
+
+    @Override
+    public int getItemCount() {
+        //TODO Return size of ArrayList to LayoutManager
+        return 0;
+    }
 }
